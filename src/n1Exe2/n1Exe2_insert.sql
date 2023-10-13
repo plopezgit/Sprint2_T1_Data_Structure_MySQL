354a43a22178be670dfc6a5790c3ea7bfff6ba86
plopezgit <p.lopez.ch.hr@hotmail.com>
2023-10-13 12:38:26 +0200

    updates n1Exe2 package fixes https://github.com/plopezgit/Sprint2_T1_Data_Structure_MySQL/issues/8
    Signed-off-by: plopezgit <p.lopez.ch.hr@hotmail.com>

--- src/n1Exe2/n1Exe2_insert.sql
+++ src/n1Exe2/n1Exe2_insert.sql
@@ -1,6 +1,7 @@
 INSERT INTO `pizza`.`province` (`id`, `province_name`) VALUES (1, 'Brio'), (2, 'Clio');
 
-INSERT INTO `pizza`.`locality` (`id`, `locality_name`) VALUES (1, 'Rul'), (2, 'Clas');
+INSERT INTO `pizza`.`locality` (`id`, `locality_name`, `province_id`) 
+VALUES (1, 'Brionte', 1), (2,'Cliotild', 2);
 
 INSERT INTO `pizza`.`client` (`id`, `client_name`, `client_surname`, `client_adress`, `client_post_code`, `client_locality_id`, `client_province_id`) 
 VALUES (1, 'Solis', 'Bol', 'adress1', '3423', 1, 2),
@@ -17,19 +18,23 @@ VALUES (1, 'Al', 'Gus', '3225454', '8875674', 'Cook', 1),
 
 INSERT INTO `pizza`.`product_category` (`id`, `product_category_name`) 
 VALUES (1, 'pizza+wings'),
-(2, 'pizza+ham');
+(2, 'pizza+ham'),
+(3, 'drink');
 
 INSERT INTO `pizza`.`product` (`id`, `product_category_id`, `product_name`, `product_description`, `product_image`, `product_price`) 
 VALUES (1, 1, 'Combo1', 'Deli', '/img.png', 12),
-(2, 2, 'Combo2', 'Promo', '/img.png', 10);
+(2, 2, 'Combo2', 'Promo', '/img.png', 10),
+(3, 3, 'Beer', 'Bruna', '/img.png', 1.5);
 
 INSERT INTO `pizza`.`order` (`id`, `order_product_id`, `order_local_date_time`, `order_type`, `order_quantity`, `order_vendor_id`, `order_client_id`, `order_invoice_number`) 
 VALUES (1, 1, '2023-01-01 00:00:00', 'Delivery', 3, 1, 1, '434'),
-(2, 2, '2023-01-01 00:00:00', 'Delivery',2, 1, 2, '435');
+(2, 2, '2023-01-01 00:00:00', 'Delivery',2, 1, 2, '435'),
+(3, 1, '2023-01-01 00:00:00', 'Delivery',2, 1, 2, '436');
 
 INSERT INTO `pizza`.`product_has_order` (`product_id`, `order_id`)
 VALUES (1, 1),
-(2, 1);
+(2, 1),
+(3, 3);
 
 INSERT INTO `pizza`.`delivery_details` (`id`, `delivery_vendor_id`, `delivery_order_id`, `delivery_local_date_time`) 
 VALUES (1, 1, 1, '2023-01-01 00:00:00'),
